bf0874156cd253783fa19c9c628d635442825cad
Stavros <snikoletopoulos@hotmail.com>
2024-04-13 15:19:50 +0300

    Remove comments

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
